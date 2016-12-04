cd09e4b751f5a864c75fb9f9b8f58a68e1cebe83
Kristaps Grinbergs <kristaps@qminderapp.com>
2016-12-04 18:32:06 +0200

    Fixes #3 #1

--- TartuWeatherProvider/Classes/TartuWeatherProvider.swift
+++ TartuWeatherProvider/Classes/TartuWeatherProvider.swift
@@ -27,7 +27,7 @@ open class TartuWeatherProvider {
       - measuredTime: Measured time in format: DD Mon YY HH:MI:SS
    
   */
-  open class func getWeatherData(completion:@escaping (_ temperature:String, _ wind:String, _ measuredTime:String) -> Void) {
+  open class func getWeatherData(completion:@escaping (_ data:Dictionary<String, String>?, _ error:Error?) -> Void) {
     Alamofire.request("http://meteo.physic.ut.ee/en/frontmain.php?m=2").responseString(completionHandler: {
       response in
       
@@ -35,8 +35,7 @@ open class TartuWeatherProvider {
           
           case .failure(let error):
           
-            debugPrint("Error: \(error)")
-            debugPrint(response)
+            completion(nil, error)
         
           case .success:
           
@@ -44,34 +43,51 @@ open class TartuWeatherProvider {
               // parse HTML file
               let document = try HTMLDocument(string: response.result.value!, encoding: String.Encoding.utf8)
 
-              var temperature = ""
-              var wind = ""
-              var measuredTime = ""
+              var weatherData:Dictionary<String,String> = [:]
 
-              // get temperature
-              if let val = document.css("td > b").first {
-                temperature = val.stringValue
+              let elements = document.css("td > b")
+              
+              // temperature
+              if let val = getValueFrom(elementsSet: elements, key: 0) {
+                weatherData["temperature"] = val
               }
               
-              // get wind strenght
-              let elements = document.css("td > b")
-                if elements.count > 2 {
-                  let w = elements[3]
-                  print("getWind = \(w.stringValue)")
-                  wind = w.stringValue
+              // humidy
+              if let val = getValueFrom(elementsSet: elements, key: 1) {
+                weatherData["humidity"] = val
+              }
+              
+              // air pressure
+              if let val = getValueFrom(elementsSet: elements, key: 2) {
+                weatherData["airPressure"] = val
+              }
+              
+              // wind
+              if let val = getValueFrom(elementsSet: elements, key: 3) {
+                weatherData["wind"] = val
+              }
+              
+              // precipitation
+              if let val = getValueFrom(elementsSet: elements, key: 4) {
+                weatherData["precipitation"] = val
+              }
+              
+              // Irradiation flux
+              if let val = getValueFrom(elementsSet: elements, key: 5) {
+                weatherData["irradiationFlux"] = val
               }
               
               // get measured time
               if let val = document.css("td > small > i").first {
-                print("\(val)")
-                measuredTime = val.stringValue
+                weatherData["measuredTime"] = val.stringValue
               }
-            
-              completion(temperature, wind, measuredTime)
+              
+              completion(weatherData, nil)
               
             } catch let error {
-              debugPrint("Error: \(error)")
-              debugPrint(response)
+            
+              completion(nil, error)
+              
             }
           
         }
@@ -93,5 +109,24 @@ open class TartuWeatherProvider {
         completion(image)
       }
     }
+  }
+  
+  /**
+    Get value from elements set
+    
+    - Parameters:
+      - elementsSet: Elements set from HTMl document
+      - key: Key of element
+      
+    - Returns: String value of element
+   
+  */
+  class func getValueFrom(elementsSet:NodeSet, key:Int) -> String? {
+    
+    if elementsSet.count > key {
+      return elementsSet[key].stringValue
+    }
+    
+    return nil
   }
 }
