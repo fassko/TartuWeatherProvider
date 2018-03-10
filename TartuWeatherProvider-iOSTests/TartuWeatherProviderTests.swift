5af4eb21b4ed9319d36655f9b08d48153e616571
Kristaps Grinbergs <fassko@gmail.com>
2018-03-10 13:20:38 +0200

    Add tests file

---
+++ TartuWeatherProvider-iOSTests/TartuWeatherProviderTests.swift
@@ -0,0 +1,103 @@
+import XCTest
+
+@testable import TartuWeatherProvider
+
+class TartuWeatherProviderTests: XCTestCase {
+
+  func testWeatherData() {
+    let expectation = self.expectation(description: "weatherData")
+    
+    var data: WeatherData?
+    
+    TartuWeatherProvider.getWeatherData { result in
+      switch result {
+      case let .success(value):
+        data = value
+        expectation.fulfill()
+      case let .failure(error):
+        print(error)
+        XCTFail("Failed to get observations")
+      }
+    }
+    
+    waitForExpectations(timeout: 5, handler: nil)
+    
+    XCTAssertNotNil(data, "Weather data should contain something")
+    XCTAssertNotNil(data?.temperature)
+    XCTAssertNotNil(data?.humidity)
+    XCTAssertNotNil(data?.airPressure)
+    XCTAssertNotNil(data?.wind)
+    XCTAssertNotNil(data?.windDirection)
+    XCTAssertNotNil(data?.precipitation)
+    XCTAssertNotNil(data?.irradiationFlux)
+    XCTAssertNotNil(data?.measuredTime)
+    XCTAssertNotNil(data?.liveImage.small)
+    XCTAssertNotNil(data?.liveImage.large)
+    XCTAssertNotNil(data?.measuredDate)
+  }
+  
+  func testQueryDataToday() {
+    getQueryData(.today)
+  }
+  
+  func testQueryDataYesterday() {
+    getQueryData(.yesterday)
+  }
+  
+  func getQueryData(_ type: QueryDataType) {
+    let expectation = self.expectation(description: "queryData\(type.description)")
+    
+    var queryData: [QueryData]?
+    
+    TartuWeatherProvider.getArchiveData(type) { result in
+      switch result {
+      case let .success(value):
+        queryData = value
+        expectation.fulfill()
+      case let .failure(error):
+        print(error)
+        XCTFail("Failed to get observations")
+      }
+    }
+    
+    waitForExpectations(timeout: 5, handler: nil)
+    
+    XCTAssertNotNil(queryData)
+    
+    guard let firstItem = queryData?[0] else {
+      XCTFail()
+      return
+    }
+
+    XCTAssertNotNil(firstItem.measuredTime)
+    XCTAssertNotNil(firstItem.temperature)
+    XCTAssertNotNil(firstItem.humidity)
+    XCTAssertNotNil(firstItem.airPressure)
+    XCTAssertNotNil(firstItem.wind)
+    XCTAssertNotNil(firstItem.windDirection)
+    XCTAssertNotNil(firstItem.precipitation)
+    XCTAssertNotNil(firstItem.uvIndex)
+    XCTAssertNotNil(firstItem.light)
+    XCTAssertNotNil(firstItem.irradiationFlux)
+    XCTAssertNotNil(firstItem.gammaRadiation)
+    XCTAssertNotNil(firstItem.measuredDate)
+    
+    guard let measuredDate = firstItem.measuredDate else {
+      XCTFail()
+      return
+    }
+    
+    var calendar = Calendar.current
+    calendar.timeZone = TimeZone(abbreviation: "EET")!
+    XCTAssertEqual(calendar.component(.hour, from: measuredDate), 00)
+    XCTAssertEqual(calendar.component(.minute, from: measuredDate), 00)
+    
+    guard let secondItemDate = queryData?[1].measuredDate else {
+      XCTFail()
+      return
+    }
+    
+    XCTAssertEqual(calendar.component(.hour, from: secondItemDate), 00)
+    XCTAssertEqual(calendar.component(.minute, from: secondItemDate), 05)
+  }
+}
